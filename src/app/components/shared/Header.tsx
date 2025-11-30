5967480b18fe90acbbf6d4d910a55c0afeafb842
Rahat Hossain <rahat1470.com@gmail.com>
2025-11-30 19:38:13 +0600

    update

---
+++ src/app/components/shared/Header.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image';
+import React from 'react';
+import Logo from './../../../assets/logo.png'
+import { ArrowUpRight } from 'lucide-react';
+const Header = () => {
+    return (
+        <div className='max-w-[1400px] m-auto bg-white py-5 my-10 mx-5 rounded-2xl'>
+            <div className="grid grid-cols-12 px-5 items-center">
+                <div className="col-span-3">
+                    <Image src={Logo} height={50} width={100} alt='Logo' />
+                </div>
+                <div className='col-span-6'>
+                    <nav>
+                        <ul className='flex gap-3 items-center justify-center'>
+                            <li><a href="#" className='text-[#606060] hover:underline'>Services</a></li>
+                            <li><a href="#" className='text-[#606060] hover:underline'>Coverage</a></li>
+                            <li><a href="#" className='text-[#606060] hover:underline'>About Us</a></li>
+                            <li><a href="#" className='text-[#606060] hover:underline'>Pricing</a></li>
+                            <li><a href="#" className='text-[#606060] hover:underline'>Blog</a></li>
+                            <li><a href="#" className='text-[#606060] hover:underline'>Contact</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className="col-span-3">
+                    <div className="flex items-center justify-end">
+                        <button className='font-semibold border border-gray-300 py-2 px-5 rounded-lg cursor-pointer'>Sign In</button>
+                        <button className='font-semibold bg-[#CAEB66] cursor-pointer py-2 px-5 rounded-lg ml-3'>Sign Up</button>
+                        <button className='bg-gray-900 cursor-pointer rounded-full ml-1 text-[#CAEB66] h-10 w-10 flex items-center justify-center'><ArrowUpRight /></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
